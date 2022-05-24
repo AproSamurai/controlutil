e6e9d0790edb2add8940b2433e91fddafdf4a5a9
Steven Smith <AproSamurai@gmail.com>
2022-05-24 11:51:01 -0700

    Changing Links to my github in case issues arise with Parent.

--- install_joycontrol.sh
+++ install_joycontrol.sh
@@ -17,8 +17,8 @@ sudo apt-get install -qy git python3-pip libglib2.0-dev libhidapi-hidraw0 libhid
 
 sudo pip3 install hid aioconsole crc8 dbus-python
 
-git clone https://github.com/Poohl/joycontrol.git
-git clone https://github.com/Jounreay/controlutil.git
+git clone https://github.com/AproSamurai/joycontrol.git
+git clone https://github.com/AproSamurai/controlutil.git
 
 sudo sed -i 's|^ExecStart=/usr/lib/bluetooth/bluetoothd.*$|ExecStart=/usr/lib/bluetooth/bluetoothd --noplugin=input|g' /lib/systemd/system/bluetooth.service
 sudo systemctl daemon-reload
